04831dcb2066fe7b9f6138c101d853d490b6086a
eliorpacarada1 <eliorpacarada1@gmail.com>
2021-02-17 19:57:31 +0100

    Final

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using CvAlternative.Models;
 
 namespace CvAlternative.Data
 {
@@ -12,5 +13,6 @@ namespace CvAlternative.Data
             : base(options)
         {
         }
+        public DbSet<CvAlternative.Models.User> User { get; set; }
     }
 }
